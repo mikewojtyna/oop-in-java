f5e48325d3d2144502ca83c3c954f5acdf23ca6f
Mike Wojtyna <mike@buildmysoftware.pro>
2019-06-13 12:11:38 +0200

    Bootstrap project

---
+++ src/main/java/pro/buildmysoftware/oopinjava/OopInJavaApplication.java
@@ -0,0 +1,13 @@
+package pro.buildmysoftware.oopinjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopInJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopInJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/buildmysoftware/oopinjava/OopInJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.buildmysoftware.oopinjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OopInJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
